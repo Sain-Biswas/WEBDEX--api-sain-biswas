4642d6f73974ee87f725de6826a338ed6838a986
Sain-Biswas <sain.biswas614@gmail.com>
2024-01-17 11:49:13 +0530

    navbar test 2

--- src/app/api/pokemon/route.js
+++ src/app/api/pokemon/route.js
@@ -1,5 +1,5 @@
-import pokemonListFetch from "@/app/Data/PKMNList";
 import { NextResponse } from "next/server";
+import pokemonListFetch from "../../Data/PKMNList";
 
 export async function GET(request) {
     const data = await pokemonListFetch();
