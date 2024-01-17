0620d276ce250413d7801a306cbe744a1a040272
Sain-Biswas <sain.biswas614@gmail.com>
2024-01-17 11:47:05 +0530

    navbar test 1

--- src/app/api/pokemon/[id]/route.js
+++ src/app/api/pokemon/[id]/route.js
@@ -1,5 +1,5 @@
-import singlePokemonFetch from "@/app/Data/SinglePKMN";
 import { NextResponse } from "next/server";
+import singlePokemonFetch from "../../../Data/SinglePKMN";
 
 export async function GET(request) {
     const id = request.url.split("pokemon/")[1];
